1fa7105107b536cc4cec9ecc6f3bd6ec90ea666e
remcoder <info@remcoder.com>
2024-12-28 20:03:48 +0100

    just testing

--- ReactorMonitor/main.lua
+++ ReactorMonitor/main.lua
@@ -1,1 +1,36 @@
 local monitor = peripheral.find("monitor")
+local reactor = peripheral.wrap("back")
+
+-- Get monitor size
+local width, height = monitor.getSize()
+
+-- Set the text scale
+monitor.setTextScale(1)
+
+-- Get Reactor Status
+local function reportReactorStatus()
+    local status = reactor.getStatus()
+    status = status and "Online" or "Offline"
+
+    if status == "Offline" then
+        term.clear()
+        term.setCursorPos(1, 1)
+        term.write("Reactor is ")
+        term.setTextColor(colors.red)
+        term.write(status)
+    else
+        term.clear()
+        term.setCursorPos(1, 1)
+        term.write("Reactor is ")
+        term.setTextColor(colors.green)
+        term.write(status)
+    end
+end
+
+-- Get Reactor Energy Stats
+
+-- Run the program
+while true do
+    reportReactorStatus()
+    sleep(1)
+end
